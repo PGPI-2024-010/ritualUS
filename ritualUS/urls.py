7e613fb067e93de0092f77729bb01c7e2d550e37
Julia Sanchez <julia.s.marquez@gmail.com>
2024-11-16 12:39:07 +0100

    feat: add Home url

--- ritualUS/urls.py
+++ ritualUS/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import Home
+
 urlpatterns = [
+    path('', Home.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls'))
 ]

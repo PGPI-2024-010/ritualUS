2b355c7388c9336ad4a2fdb923daf7a1a1539b37
Julia Sanchez <julia.s.marquez@gmail.com>
2024-11-16 12:46:13 +0100

    chore: add static urls to settings

--- ritualUS/settings.py
+++ ritualUS/settings.py
@@ -150,8 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'ritualUS/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

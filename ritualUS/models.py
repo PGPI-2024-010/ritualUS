2db2040183b017056efe7922e087146f83469aba
Julia Sanchez <julia.s.marquez@gmail.com>
2024-11-16 12:38:32 +0100

    feat: add CustomUser model modified

--- ritualUS/models.py
+++ ritualUS/models.py
@@ -1,6 +1,7 @@
 # ritualUS/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 
 
 class CustomUser(AbstractUser):
@@ -10,5 +11,21 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-    
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField()
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock_no = models.CharField(max_length=10)
+    description = models.TextField()
+    image = models.ImageField()
+    is_active = models.BooleanField(default=True)
+
+
 

6f98e3697678862fa74a5579c435758d6380dc5d
frafermot <frafermot@alum.us.es>
2024-11-17 14:26:38 +0100

    feat: Clases address, user, order, orderproduct y enum payment creados

--- ritualUS/models.py
+++ ritualUS/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import AbstractUser
 from django.shortcuts import reverse
 from enum import Enum
 
+class Payment(Enum):
+    CASH = "cash"
+    CREDIT_CARD = "credit card"
+    
+    @classmethod
+    def choices(enum):
+        return [(i.value, i.name) for i in enum]
+
 class Category(Enum):
     CANDLE = "candle"
     AROMATHERAPY = "aromatherapy"
@@ -33,6 +41,38 @@ class Product(models.Model):
     section = models.CharField(blank=True, null=True)
     factory = models.CharField(blank=True, null=True)
     product_type = models.OneToOneField(ProductType, on_delete=models.CASCADE)
+
+class Address(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.CharField()
+    city = models.CharField
+    postal_code = models.CharField()
+    street = models.CharField()
+    number = models.IntegerField
+    apartment_number = models.CharField()
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField()
+    surname = models.CharField()
+    dni = models.CharField()
+    username = models.CharField()
+    password = models.CharField()
+    email = models.EmailField()
+    telephone_number = models.CharField()
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(auto_now_add=True)
+    payment = Payment.choices()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class OrderProduct(models.Model):
+    order_id = models.OneToOneField(Order, on_delete=models.CASCADE)
+    product_id = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unity_price = models.FloatField()
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=False, null=False)

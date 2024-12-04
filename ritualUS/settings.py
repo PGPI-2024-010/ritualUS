f01ebe83c7b44b8ed4ccab8049523f2e7d68c861
javnievic <javiernieto03.jnv@gmail.com>
2024-12-04 09:36:21 +0100

    fix: add allowed hosts

--- ritualUS/settings.py
+++ ritualUS/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ritualUSApp.pythonanywhere.com', 'www.ritualUSApp.pythonanywhere.com']
 AUTH_USER_MODEL = 'ritualUS.CustomUser'
 
 # Application definition
